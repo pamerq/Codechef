e9811ac75d0e8d33282290b3fc25102fd7ba60a2
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2017-02-12 22:18:22 -0500

    Initial Commit

---
+++ Codechef/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Codechef
+//
+//  Created by Bryan Gonzales Vega on 2/12/17.
+//  Copyright © 2017 Bryan Gonzales Vega. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
